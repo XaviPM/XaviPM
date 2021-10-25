cc1a17525cf710a8faafb56fa8e3025998e05295
XaviPM <58850138+XaviPM@users.noreply.github.com>
2021-10-25 21:25:35 +0200

    Add files via upload

---
+++ SMX/appweb/http_server/procesar.php
@@ -0,0 +1,14 @@
+  <html>
+	<head></head>  
+	
+	<body style="background-color: <?php echo $_POST["color"]?>">
+		<p>El teu nom és: <?php echo $_POST["nom"]?></p>
+		<p>El teu cognom és: <?php echo $_POST["cognom"]?></p>
+		<p>La teva edad: <?php echo $_POST["edad"]?></p>
+		
+	
+	</body>		
+		
+		
+  </html>
+
